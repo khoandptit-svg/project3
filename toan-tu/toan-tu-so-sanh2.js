8890097a65d1a92c4076a2aaf65fa34ab042be7d
khoandptit-svg <khoand.ptit@gmail.com>
2026-04-01 21:30:34 +0700

    commit3

---
+++ toan-tu/toan-tu-so-sanh2.js
@@ -0,0 +1,13 @@
+// Toán tử so sánh 2
+/**
+ * ===
+ * !==
+ */
+
+var a =1 ;
+var b = '1';
+console.log(a===b); //false ;so sánh cả giá trị và kiểu dữ liệu,
+console.log(a==b); //true ;so sánh chỉ giá trị, không so sánh kiểu dữ liệu
+
+console.log(a!==b); //true ;so sánh cả giá trị và kiểu dữ liệu,
+console.log(a!=b); //false ;so sánh chỉ giá trị, không so sánh kiểu dữ liệu
---
+++ toan-tu/toan-tu-so-sanh3.js
@@ -0,0 +1,11 @@
+//hiểu hơn câu lệnh điều kiện
+//và phép so sánh
+var a = 1;
+var b = 2;
+var result = a < b;
+console.log('result:',result); //true
+if (a<b) {
+    console.log('A < B');
+} else {
+    console.log('A >= B');
+}
