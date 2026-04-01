3fd13002b6710d76da3e5cbb7ada50a3544200b2
khoandptit-svg <khoand.ptit@gmail.com>
2026-04-01 22:13:43 +0700

    commit3

--- toan-tu/toan-tu-so-sanh3.js
+++ toan-tu/toan-tu-so-sanh3.js
@@ -1,11 +1,36 @@
 //hiểu hơn câu lệnh điều kiện
 //và phép so sánh
+/* các giá trị sau đây được coi là false khi sử dụng trong điều kiện
+0
+''
+underfined
+null
+NaN
+false
+*/
+
 var a = 1;
 var b = 2;
-var result = a < b;
-console.log('result:',result); //true
-if (a<b) {
-    console.log('A < B');
-} else {
-    console.log('A >= B');
-}
+
+// var result = 'A' && 'B' && 'C' ;
+// console.log(result); //C 
+
+// var result = null && 'B' && 'C' ;
+// console.log(result); //null
+
+// var result = 'A' || 'B' || 'C' || 'D';
+// console.log(result); //A
+
+var result = 'A' || 'B' || null || 'D';
+console.log(result); //A
+
+
+// var result = a < b;
+// console.log('result:',result); //true
+
+
+// if (a<b) {
+//     console.log('Dieu kien dung');
+// } else {
+//     console.log('Dieu kien sai');
+// }
